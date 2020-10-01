5aa3180bee07b8cdc3e8b15b0ba734fb22b80acf
Amit Kaushik <kaushikabhi999@gmail.com>
2020-10-01 08:51:52 +0530

    Setup firebase configuration and axios base URL

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD5***********Umdf0ByUwmlETS8",
+  authDomain: "whatsapp-mern-*****.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-*****.firebaseio.com",
+  projectId: "whatsapp-mern-*****",
+  storageBucket: "whatsapp-mern-*****.appspot.com",
+  messagingSenderId: "7998***15677",
+  appId: "1:799886015677:web:05792f*************bd4"
+};
